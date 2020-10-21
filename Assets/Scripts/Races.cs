b67f670a50cc86c30f146578e4cbe68a3f613bab
adrianggc <agavidiacruz@gmail.com>
2020-10-21 15:52:20 -0500

    added menus and assets

---
+++ Assets/Scripts/Races.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Races : MonoBehaviour
+{
+    public string Race1Scene;
+    public string Race2Scene;
+    public string Race3Scene;
+    public string MainMenuScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void RACE1()
+    {
+        SceneManager.LoadScene(Race1Scene);
+    }
+
+    public void RACE2()
+    {
+        SceneManager.LoadScene(Race2Scene);
+    }
+
+    public void RACE3()
+    {
+        SceneManager.LoadScene(Race3Scene);
+    }
+
+    public void BACK()
+    {
+        SceneManager.LoadScene(MainMenuScene);
+    }
+}
