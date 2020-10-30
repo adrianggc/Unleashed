60355a3a6bdc114b1b43002e319602208a2887b1
adrianggc <agavidiacruz@gmail.com>
2020-10-30 16:28:35 -0500

    added Player Controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private float speed = 15.0f;
+    private float turnspeed = 45.0f;
+   
+
+    private float horizontalInput;
+    private float forwardinput;
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardinput = Input.GetAxis("Vertical");
+        //Move the Vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardinput);
+        //Rotates the car based in the horizontal input
+        transform.Rotate(Vector3.up * turnspeed * horizontalInput * Time.deltaTime);
+    }
+   
+}
--- Assets/Standard Assets/Vehicles/Car/Scripts/CarAudio.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarAudio.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using static UnityEngine.Camera;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
